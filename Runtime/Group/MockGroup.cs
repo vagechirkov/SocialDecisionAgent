cdb32fdbcd14ba69cbb5e6371dadd1f9fea53945
vagechirkov <vagechirkov@gmail.com>
2022-02-28 21:57:07 +0100

    add mock group

---
+++ Runtime/Group/MockGroup.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using System.Linq;
+using SocialDecisionAgent.Runtime.SocialAgent;
+using SocialDecisionAgent.Runtime.Task;
+using UnityEngine;
+
+namespace SocialDecisionAgent.Runtime.Group
+{
+    public class MockGroup : MonoBehaviour, IAgentGroup
+
+    {
+        [field: Tooltip("Task object")] [SerializeField] 
+        GameObject task;
+        public ITask Task { get; private set; }
+
+        public int MaxEnvironmentSteps { get; set; }
+        
+        public GameObject[] AgentGameObjects { get; private set; }
+        
+        public ISocialAgent[] Agents { get; private set; }
+        
+        public bool IsTrialRunning { get; set; }
+
+        public void InitializeAgentGroup()
+        {
+            Task = task.GetComponent<ITask>();
+
+            AgentGameObjects = GameObject.FindGameObjectsWithTag("agent");
+            Agents = AgentGameObjects.Select(a => a.GetComponent<ISocialAgent>()).ToArray();
+        }
+        
+        public virtual void GenerateTrial()
+        {
+            Task.GenerateSample();
+            foreach (var agent in Agents) agent.ResetDecisionModel(Task.Coherence);
+        }
+        
+        public float[] CollectResponsesInTheFieldOfView(GameObject agent)
+        {
+            return new float[]{};
+        }
+
+    }
+}
